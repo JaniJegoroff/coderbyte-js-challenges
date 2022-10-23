fe22ad18fd81bc3aa99c0f9e7944ee9c83a74c9b
Jani Jegoroff <jani.jegoroff@gmail.com>
2022-10-23 18:50:35 +0100

    feat: add more test cases for changing sequence

--- test/changing-sequence-spec.js
+++ test/changing-sequence-spec.js
@@ -4,7 +4,9 @@ const { expect } = require('chai');
 describe('Changing Sequence', () => {
   it('should find where array starts changing sequence', () => {
     expect(changingSequence([1, 2, 4, 6, 4, 3, 1])).to.equal(3);
+    expect(changingSequence([9, 7, 5, 3, 1, 4, 6, 8])).to.equal(4);
     expect(changingSequence([1, 2, 4])).to.equal(-1);
     expect(changingSequence([4, 3, 2])).to.equal(-1);
+    expect(changingSequence([1, 1, 1, 1])).to.equal(-1);
   });
 });
