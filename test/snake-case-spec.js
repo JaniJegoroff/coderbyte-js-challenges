c39bfe65221464a16abb8b4ab22b5a1a5d0e411c
Jani Jegoroff <jani.jegoroff@gmail.com>
2022-10-27 20:42:16 +0100

    fix: snake case test case description

--- test/snake-case-spec.js
+++ test/snake-case-spec.js
@@ -2,7 +2,7 @@ const snakeCase = require('../src/snake-case');
 const { expect } = require('chai');
 
 describe('Snake Case', () => {
-  it('should capitalize the first letter in every word', () => {
+  it('should return given string in snake case', () => {
     expect(snakeCase('Hello')).to.equal('hello');
     expect(snakeCase('BOB loves-coding')).to.equal('bob_loves_coding');
   });
